a5ed4860862ca00531335071ee1f5116bb5b55a1
gustedeveloper <219.guste@gmail.com>
2024-03-21 12:17:16 +0100

    Add functions to handle click events on main buttons and update the user interface with specific options for the store

--- script.js
+++ script.js
@@ -13,6 +13,29 @@ const goldText = document.querySelector("#goldText");
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const text = document.querySelector("#text");
+
+// Initialize buttons
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    text.innerText = "You enter the store.";
+}
+
+function goCave() {
+    console.log("You're going to the cave.");
+}
+
+function fightDragon() {
+    console.log("You're going to fight the dragon.");
+}
