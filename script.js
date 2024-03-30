6a9d6a75e862c4448d6dd2258c52bd964207bb85
gustedeveloper <219.guste@gmail.com>
2024-03-30 14:52:24 +0100

    Add functionality for an Easter egg event where the player can pick a number and potentially win gold or lose health based on the outcome and add the easterEgg function to transition to the new 'easter egg' location when triggered

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const locations = [
     {
     name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
-    "button functions": [goTown, goTown, goTown],
+    "button functions": [goTown, easterEgg, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
@@ -97,6 +97,12 @@ const locations = [
     "button text": ["Replay?", "Replay?", "Replay?"],
     "button functions": [restart, restart, restart],
     text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
+    },
+    {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ]
 
@@ -235,20 +241,20 @@ function dodge() {
 
 }
 
-function lose() {
-updateButtons(locations[5]);
-}
-
-function winGame() {
-updateButtons(locations[6]);
-}
-
 function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
     xp += monsters[fighting].level;
     goldText.innerText = gold;
     xpText.innerText = xp;
     updateButtons(locations[4]);
+}
+
+function lose() {
+updateButtons(locations[5]);
+}
+
+function winGame() {
+updateButtons(locations[6]);
 }
 
 function restart() {
@@ -261,4 +267,39 @@ xpText.innerText = xp;
 healthText.innerText = health;
 goldText.innerText = gold;
 goTown();
+}
+
+function easterEgg() {
+    updateButtons(locations[7]);
+}
+
+function pickTwo() {
+    pick(2);
+}
+
+function pickEight() {
+    pick(8);
+}
+
+function pick(guess) {
+    const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = `You picked "${guess}". Here are the random numbers:\n`;
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n";
+    }
+    if (numbers.includes(guess)) {
+        text.innerText += "Right! You win 30 gold!";
+        gold += 30;
+        goldText.innerText = gold;
+    } else {
+        text.innerText += "Wrong! You lose 10 health!";
+        health -= 10;
+        healthText.innerText = health;
+        if (health <= 0) {
+            lose();
+        }
+    }
 }
